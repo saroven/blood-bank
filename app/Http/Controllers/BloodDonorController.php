98822406d947a55288dbeae84fea0230a2df7332
= <=>
2021-11-17 00:26:07 +0600

    initizied BloodDonorController

--- app/Http/Controllers/BloodDonorController.php
+++ app/Http/Controllers/BloodDonorController.php
@@ -8,6 +8,9 @@ class BloodDonorController extends Controller
 {
     public function showDonors()
     {
-        return 'show donor';
+//        $donors = \DB::table('users')
+//            ->join('user_details', 'users.id', '=', 'user_details.user_id')
+//            ->select('users.name', 'user_details.donor_status', );
+//        return 'show donor';
     }
 }
