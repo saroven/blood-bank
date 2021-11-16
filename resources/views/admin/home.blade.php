0a50c1fd065a1b269dfcb61911a91a920b04684a
= <=>
2021-11-16 18:30:08 +0600

    displayed registered user count

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,19 @@
 @extends('admin.layout.app')
 @section('mainContent')
-    Welcome to Dashboard Home Page
+    <div class="row">
+          <div class="col-lg-3 col-6">
+              <div class="small-box bg-info">
+                  <div class="inner">
+                    <h3>{{ sizeof(\App\Models\User::all()) }}</h3>
+                    <p>Registered User</p>
+                  </div>
+                  <div class="icon">
+                    <i class="fa fa-user"></i>
+                  </div>
+                  <a href="{{ route('users') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+          </div>
+    </div>
 @endsection
 @section('siteTitle', 'Dashboard Page')
 
