165dbd904c7381ba5805fdaeee8956f829970e72
= <=>
2021-11-28 02:00:25 +0600

    updated contact page design

--- resources/views/public/contact.blade.php
+++ resources/views/public/contact.blade.php
@@ -12,30 +12,27 @@
             </div>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6 container justify-content-center">
-            <h4>মতামত/ পরামর্শ / অভিযোগ  / কোনো কিছু জানতে  লিখুন </h4><br>
+            <h4>Write for feedback / suggestions / complaints / anything</h4><br>
             <form method="POST" action="https://roktobangla.org/contact" accept-charset="UTF-8" id="regiForm" novalidate=""><input name="_token" type="hidden" value="CrxOQ70ErMARFEc4aXO0y5bjT5Y7lWWtWyKEf0Ig">
-            <input type="hidden" name="uuid" value="rokt_cont37713601d784f0285f">
             <div class="form-group">
-                <label class="control-label">আপনার নাম<span>*</span></label>
+                <label class="control-label">Your Name<span>*</span></label>
                 <input class="form-control" name="name" type="text" required
-                       placeholder="আপনার নাম"/>
+                       placeholder="Your Name"/>
             </div>
             <div class="form-group">
-                <label class="control-label">মোবাইল নম্বর (01XXXXXXXXX)<span>*</span></label>
+                <label class="control-label">Mobile Number (01XXXXXXXXX)<span>*</span></label>
                 <input class="form-control is_number" name="mobile_no" required type="number" minlength="10"
                        maxlength="13" min="0"
-                       placeholder="মোবাইল নম্বর লিখুন"/>
+                       placeholder="Your Mobile Number"/>
             </div>
             <div class="form-group">
-                <label class="control-label">মতামত/ পরামর্শ / অভিযোগ এখানে লিখুন<span>*</span></label>
+                <label class="control-label">Write comments / suggestions / complaints here<span>*</span></label>
                 <textarea class="form-control" name="message" required></textarea>
             </div>
             <div class="form-group">
-                <button type="submit" class="btn btn-danger">সম্পন্ন করুন</button>
+                <button type="submit" class="btn btn-danger">Send</button>
             </div>
             </form>
-            <h6 class="text-center" style="margin-bottom: 15px;"><a href="https://facebook.com/emedical.com.bd"><img src="../res.cloudinary.com/emedical/image/upload/v1595473222/assets/messenger_hxwz2v.png" style="width: 20px;"></a> তাৎক্ষণিক সাহায্য পেতে <a href="https://facebook.com/emedical.com.bd">ফেসবুক  পেইজে ( fb.com/emedical.com.bd) </a>
-                যোগাযোগ করুন <a href="https://facebook.com/emedical.com.bd"><img src="../res.cloudinary.com/emedical/image/upload/v1595473222/assets/messenger_hxwz2v.png" style="width: 20px;"></a> </h6>
         </div>
     </div>
 @endsection
